46e95211dbe458fe87fc33047e3252c6757d7e03
MelentQ <mejleht@yandex.ru>
2024-06-10 16:11:20 +0300

    Рефакторинг вёрстки на всём сайте - блок тарифы | 310057

---
+++ src/widgets/tariffs/index.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const containers = [...document.querySelectorAll('.js-tariffs:not(.--initialized)')];
+  containers.map((container) => {
+    let instance;
+
+    function init() {
+      if (window.matchMedia('(max-width: 767px)').matches) {
+        if (!instance) {
+          instance = new window.pts.slider.Swiper(container.querySelector('.swiper'), {
+            slidesPerView: 1,
+            spaceBetween: 16,
+            autoHeight: true,
+            initialSlide: container.getAttribute('data-initial-slide') || 0,
+            loop: true,
+            pagination: {
+              el: container.querySelector('.slider-pagination'),
+              ...window.pts.slider.paginationOptions,
+            },
+            on: {
+              init: window.pts.slider.hideLoader,
+            },
+          });
+        }
+      } else {
+        const loader = container.querySelector('.loader');
+        if (loader) {
+          loader.classList.remove('loader');
+        }
+
+        if (instance) {
+          instance.destroy();
+          instance = undefined;
+        }
+      }
+    }
+
+    init();
+
+    window.addEventListener('resize', init);
+
+    container.classList.add('--initialized');
+
+    return instance;
+  });
+});
